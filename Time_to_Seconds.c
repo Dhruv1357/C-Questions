4028bfa837f4dc72bdcf4becb848815e573d6c4b
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-02 01:54:54 +0530

    Added pointer programs

---
+++ Time_to_Seconds.c
@@ -0,0 +1,29 @@
+// Problem - Convert Time to Seconds
+// Platform - HackerRank
+// Link - https://www.hackerrank.com/contests/peacemakers-25b-blr/challenges/convert-time-into-seconds/submissions/code/1400694006
+// Summary - Converting hours,minutes and seconds to only seconds.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int toSeconds(int hours, int minutes, int seconds)
+{
+    hours=hours*3600;
+    minutes=minutes*60;
+    int res=hours+minutes+seconds;
+    printf("Total seconds: %d",res);
+    return res;
+}
+int main() 
+{
+    int hours;
+    int minutes;
+    int seconds;
+    scanf("%d %d %d",&hours,&minutes,&seconds);
+    
+    toSeconds(hours,minutes,seconds);
+
+    return 0;
+}
