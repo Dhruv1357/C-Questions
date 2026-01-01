546bdcd74adfb8ba8bd1c6fb0cac07f287116fc6
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-02 01:56:52 +0530

    Added pointer programs

---
+++ Sum_Last_Digits.c
@@ -0,0 +1,26 @@
+// Problem - Sum Last Digits
+// Platform - HackerRank
+// Link - https://www.hackerrank.com/contests/peacemakers-25b-blr/challenges/sum-of-last-digits-of-two-numbers/submissions/code/1400739598
+// Summary - Finding the sum of the last digits of the given two numbers.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int sumLastDigits(unsigned int a, unsigned int b){
+    short last1=a%10;
+    short last2=b%10;
+    short res=(last1+last2);
+    printf("The sum of last digits is: %hd",res);
+    return res;
+}
+int main() 
+{
+    unsigned int a;
+    unsigned int b;
+    scanf("%u %u",&a,&b);
+    sumLastDigits(a,b);
+  
+    return 0;
+}
