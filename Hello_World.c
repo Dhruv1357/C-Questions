4f9a6cbd0d24c4b19fb0a45bd4d268bc3d2e6f27
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-01 23:26:17 +0530

    Added C programs

---
+++ Hello_World.c
@@ -0,0 +1,15 @@
+// Problem - Let's begin with a greeting - Hello World!
+// Platform - HackerRank
+// Link - https://www.hackerrank.com/contests/peacemakers-25b-blr/challenges/challenge-1-a-hello-world/submissions/code/1400635815
+// Summary - Printing Hello World!
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    printf("Hello World!");
+    return 0;
+}
