95dc409265abc82cb272649052549dd675fc998f
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-08 21:43:00 +0530

    Added pointer programs

---
+++ Swap_two_numbers.c
@@ -0,0 +1,16 @@
+/*
+Problem - Swap two numbers
+Platform - HackerRank
+Summary - Swapping the given two numbers and printing their swapped values
+*/
+
+#include <stdio.h>
+
+int main() {
+    long a;
+    long b;
+    scanf("%ld %ld",&a,&b);
+    printf("a = %ld\nb = %ld",b,a);
+   
+    return 0;
+}
