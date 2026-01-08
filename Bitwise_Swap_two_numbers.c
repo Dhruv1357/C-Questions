c24a70cda93a4c8a431d67676c534038201cb8e7
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-09 02:12:51 +0530

    Added pointer programs

---
+++ Bitwise_Swap_two_numbers.c
@@ -0,0 +1,18 @@
+/*
+Problem - Bitwise swap two numbers
+Platform - HakcerRank
+Summary - Swapping two numbers using bitwise operations
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d\n%d",&a,&b);
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    printf("%d\n%d",a,b);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
