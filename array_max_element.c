3a9363b166de21c3e9da91fdb4332c9f20f350e5
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-26 18:42:29 +0530

    Added pointer programs

---
+++ array_max_element.c
@@ -0,0 +1,33 @@
+/*
+Problem - Arrays Find the Maximum Element in an Array
+Platform - HackerRank
+Summary - Finding the largest element of a given array
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int res=0;
+    
+    for(int i=0;i<=n-1;i++)
+        scanf("%d ",&a[i]);
+    
+    if(res!=a[0])
+        res=a[0];
+    
+    for(int i=0;i<=n-1;i++)
+    {
+        if(res<a[i])
+            res=a[i];
+    }
+    
+    printf("%d",res);
+  
+    return 0;
+}
