69e9d641e169248a73071d1820fa7c154fae9f00
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-29 23:00:36 +0530

    Added pointer programs

---
+++ arrays_replace_negative_with_0.c
@@ -0,0 +1,37 @@
+/*
+Problem - Arrays Replace Every Negative Number with 0
+Platform - HackerRank
+Summary - Replacing every negative element of the array with a 0
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<0)
+        {
+            a[i]=0;
+        }
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+ 
+    return 0;
+}
