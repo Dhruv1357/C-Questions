10b84533f5c2d1b624bdbd12b4668eb6fc268951
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-01-30 22:42:14 +0530

    Added pointer programs

---
+++ is_array_sorted.c
@@ -0,0 +1,35 @@
+/*
+Problem - Arrays Check if Array is Sorted
+Platform - HackerRank
+Summary - Checking if the array is sorted in either ascending or descending order or not
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            printf("Not Sorted");
+            return 0;
+        }
+    }
+    
+    printf("Sorted");
+
+    return 0;
+}
