e63d4dc3adeea857d953444c593f0fba35adac27
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-02-04 22:52:13 +0530

    Added pointer programs

---
+++ printing_repeated_numbers.c
@@ -0,0 +1,21 @@
+/*
+Problem - Repeated Number pattern
+Summary - Printing a number pattern where numbers are repeated
+*/
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
