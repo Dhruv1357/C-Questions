4a5493dd59fc02eb65b88b6b327d69dbe21d8c0a
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-09 23:10:45 +0530

    fix styling

--- Pattern_Isosceles_Triangle.c
+++ Pattern_Isosceles_Triangle.c
@@ -9,8 +9,7 @@ Summary - Printing an isosceles triangle pattern based on the number of rows giv
 int main() {
     int n;
     scanf("%d",&n);
-    if(n>1)
-    {
+    if(n>1){
         for(int i=1;i<=n;i++)
         {
             for(int k=1;k<=n-i;k++)
