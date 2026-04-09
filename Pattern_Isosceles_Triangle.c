90f07d2eef0aebc9500f8b90f61f0be87e4e3aab
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-09 23:11:09 +0530

    fix styling

--- Pattern_Isosceles_Triangle.c
+++ Pattern_Isosceles_Triangle.c
@@ -10,8 +10,7 @@ int main() {
     int n;
     scanf("%d",&n);
     if(n>1){
-        for(int i=1;i<=n;i++)
-        {
+        for(int i=1;i<=n;i++){
             for(int k=1;k<=n-i;k++)
             {
                 printf(" ");
