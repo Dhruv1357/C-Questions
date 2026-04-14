eb2e30d07c612acbe9b1e96badd754be8f1941e4
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-14 20:19:20 +0530

    remove redundant line

--- Numbers_Harshad_Number.c
+++ Numbers_Harshad_Number.c
@@ -11,8 +11,7 @@ Summary - Classifying whether the given number is a Harshad number or not
 
 int main() {
     
-    int sum=0;
-    int n;
+    int sum=0, n;
     int new;
     int updated;
     scanf("%d",&n);
